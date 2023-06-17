17f904b0fb298d50a2e04ba548a3350e9e6348d2
Risan Mahfuz <102344404+Risan99@users.noreply.github.com>
2023-06-17 20:42:16 +0600

    Update bisection.cpp

--- bisection.cpp
+++ bisection.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-double f(double x)
+
+double f(double x) //Given polynomial
 {
     return x * x * x + x * x + 2 * x - 4;
 }
@@ -8,19 +9,19 @@ int main()
 {
     double a, b, e;
     cout << "Enter doubleitial guess: ";
-    cin >> a >> b;
+    cin >> a >> b;   //Initial Guess
     cout << "Enter precision: ";
-    cin >> e;
-    if (f(a) * f(b) > 0)
+    cin >> e;       // Tolarble error  
+    if (f(a) * f(b) > 0)  //Checking weather the root is in the bound
         cout << "Root maybe not in the range.\n";
     else
     {
         double mid;
         double error = 100;
         int i = 1;
-        while (error > e)
+        while (error > e)  // As long as error greater than tolarable error
         {
-            mid = (a + b) / 2;
+            mid = (a + b) / 2; 
             cout << "Root after " << i++ << "th iteration :" << mid << '\n';
             if (f(mid) == 0.0)
                 break;
