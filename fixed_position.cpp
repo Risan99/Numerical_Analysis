1257c7305f897de19e2b263594529752b3b93d13
Risan Mahfuz <102344404+Risan99@users.noreply.github.com>
2023-06-18 01:28:02 +0600

    Update fixed_position.cpp

--- fixed_position.cpp
+++ fixed_position.cpp
@@ -1,33 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// double f(double x) // Given polynomial
+// {
+//     return x * x * x + x * x + 2 * x - 4;
+// }
 double g(double x)
 {
     return (-x * x * x * x + 3 * x * x * x - 5 * x * x + 5) / 5;
 }
-
-double fixedPos(double x0, double es, double &ea, int &iter)
+double fixedposition(double x, double er)
 {
-    iter = 1e9;
-    double xrold, xr = x0;
-    while (iter--)
+    double x2;
+    double error = 10000000;
+    for (int i = 1;; i++)
     {
-        xrold = xr;
-        xr = g(xrold);
+        x = x2;
+        x2 = g(x);
 
-        if (xr != 0)
+        if (x2 != 0)
         {
-            ea = fabs((xr - xrold) / xr) * 100;
+            error = fabs((x2 - x) / x2);
+            // if(error<=-1) error*=(-1);
         }
-        if (ea < es)
-        {
+        cout << "After " << i << "th step "
+             << "Root= " << x2 << " Error= " << error << endl;
+        if (error < er)
             break;
-        }
-        cout << "After " << 1e9 - iter << "th step "
-             << "Root= " << xr << " Error= " << ea << endl;
     }
+    return x2;
+}
 
-    return xr;
+int main()
+{
+    double x, er;
+    cin >> x >> er;
+    double root = fixedposition(x, er);
+    cout << root << '\n';
+    return 0;
+}
+
+
+
+
+// second way almost similar
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// double g(double x)
+// {
+//     return (-x * x * x * x + 3 * x * x * x - 5 * x * x + 5) / 5;
+// }
+
+// double fixedPos(double x0, double es, double &ea, int &iter)
+// {
+//     iter = 1e9;
+//     double xrold, xr = x0;
+//     while (iter--)
+//     {
+//         xrold = xr;
+//         xr = g(xrold);
+
+//         if (xr != 0)
+//         {
+//             ea = fabs((xr - xrold) / xr) * 100;
+//         }
+//         if (ea < es)
+//         {
+//             break;
+//         }
+//         cout << "After " << 1e9 - iter << "th step "
+//              << "Root= " << xr << " Error= " << ea << endl;
+//     }
+
+//     return xr;
 }
 
 int main()
